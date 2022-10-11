ad916e8a5741e77e23ca797a8aad02dbf2ea7bc9
wlv84 <wenlv84hb@gmail.com>
2022-10-11 12:13:08 -0400

    Update R Syntax

--- test.R
+++ test.R
@@ -1,1 +1,35 @@
-# R syntax for CI/CD testing
+library("tidyverse")
+library("purrr")
+library("openxlsx")
+
+# Requests:
+# 1. Create df of 10 columns, x1-x10, and populate with random integers of 1-10
+# 2. Create 10 df's by summarizing for counts from each column using tidy eval
+# 3. Output resulting df's into excel, one df per tab
+
+# create the original df
+set.seed(1)
+vars <- paste0("x", 1:10)
+A <- sample(1:10, 1000, TRUE)
+A <- matrix(A, 100, 10) %>% as.data.frame() %>% rename_with(~ vars)
+names(A)
+
+# create 10 summary df's by each column
+a <- list()
+for (i in seq_along(vars)) {
+  x <- A %>% 
+    group_by(!!!rlang::syms(vars[i])) %>% 
+    summarize(freq = n(), .groups = "drop")
+  a[[i]] <- x
+  print(names(x))
+  rm(x)
+}
+
+# Output with a note
+names(a) <- vars
+write.xlsx(a, paste("output", "test.xlsx", sep = "/"))
+print("Mission Accomplished!!!")
+
+
+
+
